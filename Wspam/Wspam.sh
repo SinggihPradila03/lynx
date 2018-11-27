1f5a4539b58cf97abc6479b837f3746d0a1eb0fb
SinggihPradila03 <40047166+SinggihPradila03@users.noreply.github.com>
2018-11-27 15:44:26 +0700

    Add files via upload

---
+++ Wspam/Wspam.sh
@@ -0,0 +1,53 @@
+#SINGGIH WHYTHEFUCK
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo "##################################"|lolcat
+echo "/ TOOLS : SPAM LY                /"|lolcat
+echo "/ CODED : KAK.OTX                /"|lolcat
+echo "/ TEAM  : 407 Authentic eXploit  /"|lolcat
+echo "##################################"|lolcat
+
+echo "daftar tools spam"
+echo "[1] Bom Chat Whatsapp"
+echo "[2] Spam Telpon"
+echo "[3] Keluar"
+read -p "PILIH SALAH SATU : " bro
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+php mdr2.php
+sh Wspam.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 01 ]
+then
+clear
+php mdr1.php
+sh Wspam.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+echo "\033[31;1mWE ARE 407 AUTHENTIC EXPLOIT"
+sleep 1
+echo "\033[32;1mTerimakasih Sudah Menggunakan Tools ini"
+sleep 1
+echo "\033[35;1mSelamat Beraktivitas"
+sleep 1
+exit
+fi
